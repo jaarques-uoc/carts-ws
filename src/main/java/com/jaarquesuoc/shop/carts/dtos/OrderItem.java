49266e9b8fc0a7accad05e1cb315a488f40afb13
Jordi <jaarques@uoc.edu>
2019-05-12 19:47:05 +0200

    Enabling constructors for serialisation

--- src/main/java/com/jaarquesuoc/shop/carts/dtos/OrderItem.java
+++ src/main/java/com/jaarquesuoc/shop/carts/dtos/OrderItem.java
@@ -1,10 +1,14 @@
 package com.jaarquesuoc.shop.carts.dtos;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderItem {
 
     private Product product;
